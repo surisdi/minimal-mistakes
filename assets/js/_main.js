b2fa80bd6ba8a6c8ac587df621de24b2ae91afda
didacsuris <didac8989@gmail.com>
2018-07-11 12:56:06 -0400

    Page under construction

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -28,7 +28,7 @@ $(document).ready(function() {
   var stickySideBar = function() {
     var show =
       $(".author__urls-wrapper button").length === 0
-        ? $(window).width() > 128 // width should match $large Sass variable
+        ? $(window).width() > 1024 // width should match $large Sass variable
         : !$(".author__urls-wrapper button").is(":visible");
     if (show) {
       // fix
